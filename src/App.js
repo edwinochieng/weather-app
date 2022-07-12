2dd0a368944cd44810231f9b8cb777da84358ad8
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-07-13 02:58:27 +0300

    center components on the page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import City from "./components/City";
+import Weatherinfo from "./components/Weatherinfo";
 
 function App() {
   return (
-    <div>
-      Hello
+    <div className="flex flex-col items-center h-screen bg-cyan-300 bg-opacity-50">
+      <City/>
+      <Weatherinfo/>
     </div>
   );
 }
