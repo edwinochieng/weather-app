48e58d8b544c9bcf0f840d3df7960f12556db3fb
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-07-14 23:45:56 +0300

    design weather card

---
+++ src/components/Weatherinfo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Weatherinfo() {
+  return (
+    <div className='bg-teal-100 h-64 w-96 bg-white border border-solid rounded-lg '>
+    <h1>Paris</h1>
+    <h2>16</h2>
+
+
+      
+    </div>
+  )
+}
+
+export default Weatherinfo;
