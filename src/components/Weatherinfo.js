ebdc9a75e592fc1506371fc4937803779ea28322
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-07-15 04:39:40 +0300

    display data from openweathermap api

--- src/components/Weatherinfo.js
+++ src/components/Weatherinfo.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
-function Weatherinfo() {
+function Weatherinfo(props) {
+   
   return (
-    <div className='bg-teal-100 h-64 w-96 bg-white border border-solid rounded-lg '>
-    <h1>Paris</h1>
-    <h2>16</h2>
+    <div className='bg-gray-50 bg-opacity-100 h-96 w-80 rounded-3xl mr-16 pt-16 flex flex-col items-center'>
+    <div className='pr-4'>
+    <h1 className=''>{props.city}</h1>
+    <h1 className='font-bold font-sans text-3xl'>{props.temperature}°C </h1>
+    
+    <img src= 'http://openweathermap.org/img/wn/10d@2x.png' alt = {props.description} />
+    </div>
+    
+    <div className='font-semibold font-sans text-md py-4' >
+    <p className='pb-4'>{props.description}</p>
+    <p>Humidity: {props.humidity} %</p>
+    <p>Pressure: {props.pressure} Pa</p>
+   
+    </div>
+   
 
 
       
