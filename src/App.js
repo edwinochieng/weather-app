2a6a8e59eb1a56675e290993f2049561692bd1b1
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-07-16 03:28:27 +0300

    link user input to api call

--- src/App.js
+++ src/App.js
@@ -10,23 +10,26 @@ function App() {
 
   const [weather,setWeather] =useState([]);
   const [conditions, setConditions] = useState({});
-  const getWeather = async() =>{
+  
+  const getWeather = async(cityName) =>{
     const apiKey = process.env.REACT_APP_API_KEY;
-    const city = 'Paris';
+    const city = cityName
     const api = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid='+ apiKey + '&units=metric');
     const data = await api.json();
     setWeather(data.weather[0]);
     setConditions(data.main)
-    console.log(data);
+    
     
   
   }
   return (
     <div className="flex flex-col items-center h-screen bg-cyan-300 bg-opacity-50">
-      <City/>
+      <City
+        onAdd = {getWeather}
+      />
       <Weatherinfo
-        description = {weather.description}
         
+        description = {weather.description}
         temperature = {conditions.temp}
         humidity = {conditions.humidity}
         pressure = {conditions.pressure}
