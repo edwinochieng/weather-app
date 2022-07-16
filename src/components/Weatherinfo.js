02d06ada286c1e13965a565a28a3ce4f9fad7491
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-07-16 03:30:19 +0300

    add weather icon

--- src/components/Weatherinfo.js
+++ src/components/Weatherinfo.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React from "react";
 
 function Weatherinfo(props) {
-   
+  const imgUrl = `http://openweathermap.org/img/wn/${props.icon}@2x.png`;
+
   return (
-    <div className='bg-gray-50 bg-opacity-100 h-96 w-80 rounded-3xl mr-16 pt-16 flex flex-col items-center'>
-    <div className='pr-4'>
-    <h1 className=''>{props.city}</h1>
-    <h1 className='font-bold font-sans text-3xl'>{props.temperature}°C </h1>
-    
-    <img src= 'http://openweathermap.org/img/wn/10d@2x.png' alt = {props.description} />
-    </div>
-    
-    <div className='font-semibold font-sans text-md py-4' >
-    <p className='pb-4'>{props.description}</p>
-    <p>Humidity: {props.humidity} %</p>
-    <p>Pressure: {props.pressure} Pa</p>
-   
-    </div>
-   
+    <div className="bg-teal-50 h-96 w-80 rounded-3xl mr-16 pt-16 flex flex-col items-center">
+      <div className="">
+        <h1 className="">{props.city}</h1>
+        <h1 className="font-bold font-sans text-3xl">{props.temperature}°C </h1>
+      </div>
 
+      <div>
+        <img
+          className="h-40 w-56 brightness-50 contrast-200"
+          src={imgUrl}
+          alt={props.description}
+        />
+      </div>
 
-      
+      <div className="font-semibold font-sans text-md pb-2">
+        <p className="pb-4">{props.description}</p>
+        <p>Humidity: {props.humidity} %</p>
+        <p>Pressure: {props.pressure} Pa</p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Weatherinfo;
